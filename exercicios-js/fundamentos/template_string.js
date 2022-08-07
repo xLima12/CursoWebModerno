963f80e512729c2476bb9099cae659c198a9eb1a
xLima12 <90412987+xLima12@users.noreply.github.com>
2022-08-07 19:08:08 -0300

    exercicios booleanos

---
+++ exercicios-js/fundamentos/template_string.js
@@ -0,0 +1,12 @@
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + '!';
+const template = `
+Olá
+${nome}!`
+console.log(concatenacao, template);
+
+//expressoes...
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = s => s.toUpperCase();
+console.log(`Ei... ${up('cuidado')}!`);
