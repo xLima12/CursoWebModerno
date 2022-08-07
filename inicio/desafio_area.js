bcc905e6c8f64da64c41a23d1c0923f698db08d4
xLima12 <90412987+xLima12@users.noreply.github.com>
2022-08-07 15:59:07 -0300

    Desafios iniciais

---
+++ inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// Valor da área
+
+//Sem usar as operações matemáticas do JavaScript
+let raio = 5;
+const PI = 3.14;
+let area = PI * raio * raio;
+console.log('O valor da área é: ' + area + 'm²');
+
+//Com as operações matemáticas do JavaScript
+area = Math.PI * Math.pow(5, 2);
+console.log('O valor da área é: ' + area + 'm²')
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,18 @@
+//Trocar o valor das variaveis
+// Sem usar recursos do JavaScript
+let a = 7;
+let b = 94;
+
+let c = a;
+
+a = b;
+b = c;
+
+console.log('A variavel a vale ' + a + ' e a variavel b vale ' + b)
+
+// Usando recursos do JavaScript
+//let a = 7;
+//let b = 94;
+
+//[a, b] = [b, a]
+//console.log('A variavel a vale ' + a + ' e a variavel b vale ' + b)
