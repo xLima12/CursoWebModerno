4b6d67ea045a594c6c7754d2c36db0f8d5d912ae
xLima12 <90412987+xLima12@users.noreply.github.com>
2022-08-08 16:24:06 -0300

    atualização

--- exercicios-js/fundamentos/math.js
+++ exercicios-js/fundamentos/math.js
@@ -1,6 +1,16 @@
+//Forma antiga de usar exponenciação usando Math.pow()
 const raio = 5.6;
 const area = Math.PI * Math.pow(raio, 2);
 
 console.log(area);
 console.log(typeof Math);
 console.log(typeof area);
+
+//exponênciação nova forma de usar do ES7
+
+let exp = 2 ** 3
+console.log(exp)
+
+let base = 2
+base **= 3
+console.log(base)
