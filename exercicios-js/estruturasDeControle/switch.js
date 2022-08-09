79512bd43edae43a20d52823ffbf346f6e40307e
xLima12 <90412987+xLima12@users.noreply.github.com>
2022-08-09 10:59:45 -0300

    att

--- exercicios-js/estruturasDeControle/switch.js
+++ exercicios-js/estruturasDeControle/switch.js
@@ -1,7 +1,6 @@
 const imprimirResultado = function (nota) {
     switch (Math.floor(nota)){
-        case 10:
-        case 9:
+        case 10: case 9:
             console.log('Quadro de honra')
             break
         case 8: case 7:
