837f0ac403499ffd635df8ffcaac0a2f5aa9b814
xLima12 <90412987+xLima12@users.noreply.github.com>
2022-08-10 12:46:32 -0300

    autalizando

--- exercicios-js/funcoes/desafioFuncaoConstrutora.js
+++ exercicios-js/funcoes/desafioFuncaoConstrutora.js
@@ -1,9 +1,10 @@
 function Pessoa(nome) {
+    this.nome = nome
 
     this.falar = function () {
-        console.log(`Meu nome é ${nome}`)
+        console.log(`Meu nome é ${this.nome}`)
     }
-    
+
 }
 
 const p1 = new Pessoa('João')
