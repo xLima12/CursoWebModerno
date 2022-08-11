737369324ee5e51027ae60cbf75c658c48d6b9f6
xLima12 <90412987+xLima12@users.noreply.github.com>
2022-08-11 08:57:10 -0300

    calcJurosComposto

--- exercicios-js/funcoes/Exercicios/ex6.js
+++ exercicios-js/funcoes/Exercicios/ex6.js
@@ -1,11 +1,17 @@
-function jurosSimples(capitalInicial, taxaJuros, tempoAplicacao){
+function jurosSimples(capitalInicial, taxaJuros, tempoAplicacao) {
     const calcJurosSimples = (capitalInicial * taxaJuros) * tempoAplicacao
     const valorFinalCalc = calcJurosSimples + capitalInicial
     return console.log(`O valor da aplicação será ${valorFinalCalc}`)
 }
 
-jurosSimples(18000, 0.06, 3)
+jurosSimples(1200, 0.02, 14)
 
-function jurosComposto(capitalInicial, taxaJuros, tempoAplicacao){
+function jurosComposto(capitalInicial, taxaJuros, tempoAplicacao) {
+
+const calcJurosComposto = capitalInicial * (1 + taxaJuros) ** tempoAplicacao
+
+console.log(calcJurosComposto.toFixed(2))
 
 }
+
+jurosComposto(1000, 0.04, 14)
