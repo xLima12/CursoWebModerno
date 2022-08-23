4a89ef24f4612d49cdae33ffea70866b0c842333
xLima12 <90412987+xLima12@users.noreply.github.com>
2022-08-23 12:57:33 -0300

    Calculo overall Time Utimate Team FIFA

---
+++ fut/calcOverTeam.js
@@ -0,0 +1,22 @@
+function getOverRandom(over) {
+
+    let calcOver = 0
+    let average = 0
+    function calc() {
+        for (let count = 1; average <= over; count++) {
+            let randomOver = Math.random() * (90 - 75 + 1) + 75
+            if (average <= over) {
+                average += (calcOver + randomOver) / 11
+                if(average > over){
+                    break
+                } else {
+                    console.log(`Jogador ${count}, Overall ${Math.floor(randomOver)}, Overall Team ${Math.floor(average)}`)
+                }
+            }
+        }
+    }
+
+    return calc()
+}
+
+getOverRandom(88)
